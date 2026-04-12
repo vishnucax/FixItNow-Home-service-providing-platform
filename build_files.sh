7bedd4b14f6eae80d2b4103f645e70c887f41b96
Vishnu K <vishxsims@gmail.com>
2026-04-12 12:13:43 +0530

    Add data seeding script to initialize demo accounts on Vercel

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@ echo "BUILD START"
 pip install -r requirements.txt
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+python seed_data.py
 python manage.py collectstatic --noinput --clear
 echo "BUILD END"
