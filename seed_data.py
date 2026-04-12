690f5993cce4514a05aa0f0ae4a6700badc740dc
Vishnu K <vishxsims@gmail.com>
2026-04-12 12:51:45 +0530

    Update seed_data.py

--- seed_data.py
+++ seed_data.py
@@ -3,16 +3,16 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomeServiceManagement.settings')
 django.setup()
 from django.contrib.auth.models import User
-from home_service.models import Status, Customer, Service_Man, City
+from home_service.models import *
 def seed():
-    if not User.objects.filter(username="admin").exists():
-          User.objects.create_superuser("admin", "admin@example.com", "admin")
-    f not User.objects.filter(username="serviceprovider").exists():
-              u = User.objects.create_user("serviceprovider", "sp@example.com", "serviceprovider")
-              Service_Man.objects.create(user=u, name="SP", contact="1", address="A", id_card="I")
-        f not User.objects.filter(username="user").exists():
-                  u = User.objects.create_user("user", "u@example.com", "user")
-                  Customer.objects.create(user=u, name="U", contact="2", address="B")
-    f __name__ == "__main__":
-                  seed()
-                
+      if not User.objects.filter(username='admin').exists():
+              User.objects.create_superuser('admin', 'a@a.com', 'admin')
+            if not User.objects.filter(username='serviceprovider').exists():
+                    u = User.objects.create_user('serviceprovider', 's@s.com', 'serviceprovider')
+                    Service_Man.objects.create(user=u, name='S', contact='1', address='A', id_card='I')
+                  if not User.objects.filter(username='user').exists():
+                          u = User.objects.create_user('user', 'u@u.com', 'user')
+                          Customer.objects.create(user=u, name='U', contact='2', address='B')
+                      if __name__ == "__main__":
+                            seed()
+                          
